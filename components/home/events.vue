40385615b430513fb068212092c0c9e56b609fc0
smetcalf <smetcalf@smetcalfs-MacBook-Pro.local>
2019-12-04 18:54:52 -0500

    testing mobile

--- components/home/events.vue
+++ components/home/events.vue
@@ -2,25 +2,21 @@
   <div id="events" class="padding" ref="events" data-scroll-section>
     <div class="content">
 
-      <v-box :styles="{top: '36vw',right:'0px',zIndex: 1}" speed="-.5">
-        <v-text :text="data.title" class="title big" :play="animateTitle"/>
-      </v-box>
+      <div class="box title-1" data-scroll data-scroll-speed="-.5">
+        <v-text :text="data.title" class="title big" :play="animateTitle" />
+      </div>
 
-      <v-box :styles="{top: '20vw',left: '0px',width: '45vw',height: '60vw'}" speed="-.5">
-        <v-image :image="data.image1"/>
-      </v-box>
+      <div class="box image-1" data-scroll data-scroll-speed="-.5">
+        <v-image :image="data.image1" />
+      </div>
 
-      <v-box :styles="{top: '2vw',right: '2vw',width: '25vw',height: '30vw'}" speed=".5">
+      <div class="box image-2" data-scroll data-scroll-speed=".5">
         <v-image :image="data.image2"/>
-      </v-box>
+      </div>
 
-      <v-box :styles="{top: '67vw',right: '0px',width: '30vw',height: '30vw'}" speed=".25">
+      <div class="box image-3" data-scroll data-scroll-speed=".25">
         <v-image :image="data.image3"/>
-      </v-box>
-
-      <v-box :styles="{top: '90vw',left: '10vw',zIndex: 1}" speed=".5">
-        description
-      </v-box>
+      </div>
 
     </div>
   </div>
@@ -59,5 +55,52 @@ export default {
 #events .content{
   position: relative;
   height: 100%;
+}
+
+#events .box{
+  position: absolute;
+}
+
+#events .title-1{
+  top: 36vw;
+  right:0px;
+  z-index: 1
+}
+
+#events .image-1{
+  top: 20vw;
+  left: 0px;
+  width: 45vw;
+  height: 60vw
+}
+
+#events .image-2{
+  top: 2vw;
+  right: 2vw;
+  width:25vw;
+  height: 30vw
+}
+
+#events .image-3{
+  top: 67vw;
+  right: 0px;
+  width: 30vw;
+  height: 30vw
+}
+
+@media screen and (max-width: $mobile){
+  #events{
+    height: auto;
+    padding: 0px;
+  }
+
+  #events .box{
+    position: relative;
+    top: auto;
+    left: auto;
+    right: auto;
+    height: 100vh;
+    width: 100%;
+  }
 }
 </style>
